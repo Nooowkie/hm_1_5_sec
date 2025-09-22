65bf90b395cb69d9ff030e91fd43b1ee3a50a799
Nooowkie <alirahmanzanov2323@gmail.com>
2025-09-22 18:48:51 +0600

    hm

---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+function Content({ title, text }) {
+  return (
+    <div className="content">
+      <h2>{title}</h2>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,15 @@
+import Content from "../components/Content.jsx";
+
+function MainPage() {
+  return (
+    <div>
+      <h1>🍕🍕🍕</h1>
+      <Content
+        title="Hello World!" 
+        text="agugagaga" 
+      />
+    </div>
+  );
+}
+
+export default MainPage;
